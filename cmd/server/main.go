482abb442df359270639aa8537297b1c8e5ce648
Irawan Kilmer <irawankillmer@gmail.com>
2024-08-09 22:40:48 +0700

    cors middleware in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"log"
+	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/irawankilmer/lms_backend/config"
 	"github.com/irawankilmer/lms_backend/internal/db"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -20,6 +23,22 @@ func main() {
 
 	// Initialize Gin
 	r := gin.Default()
+
+	// CORS middleware
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Logger middleware
+	r.Use(gin.Logger())
+
+	// Recovery middleware
+	r.Use(gin.Recovery())
 
 	// Define a simple ping endpoint
 	r.GET("/ping", func(c *gin.Context) {
