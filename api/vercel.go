b61d378f2adcccabf81f5d5467d499e4ac1afd20
Irawan Kilmer <irawankillmer@gmail.com>
2024-08-12 18:50:28 +0700

    devloy to vercel

--- api/vercel.go
+++ api/vercel.go
@@ -1,14 +1,20 @@
-package main
+package api
 
 import (
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/irawankilmer/lms_backend/config"
 	_ "github.com/irawankilmer/lms_backend/docs"
 	"github.com/irawankilmer/lms_backend/internal/db"
 	"github.com/irawankilmer/lms_backend/internal/handler"
 	"github.com/irawankilmer/lms_backend/internal/router"
 	"github.com/irawankilmer/lms_backend/internal/service"
+)
+
+var (
+	app *gin.Engine
 )
 
 // @title Learning Management System API
@@ -22,7 +28,7 @@ import (
 // @in header
 // @name Authorization
 
-func main() {
+func init() {
 	// Load configuration
 	cfg, err := config.LoadConfig()
 	if err != nil {
@@ -42,4 +48,9 @@ func main() {
 
 	// Start server
 	r.Run(":" + cfg.AppPort)
+}
+
+// vercel net/http entrypoint
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
 }
